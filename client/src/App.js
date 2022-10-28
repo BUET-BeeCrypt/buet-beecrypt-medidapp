20b936d652a0ac27fefa866222bda800baa8f39c
Tamim Ehsan <tamimehsan99@gmail.com>
2022-10-28 16:58:21 +0600

    IPFS - Pinata File Upload

---
+++ client/src/App.js
@@ -0,0 +1,65 @@
+import logo from './logo.svg';
+import { useState } from 'react';
+import './App.css';
+import axios from 'axios'
+
+function App() {
+  const [fileImg, setFileImg] = useState(null);
+
+  const sendFileToIPFS = async (e) => {
+    if (fileImg) {
+        try {
+
+            const formData = new FormData();
+            formData.append("file", fileImg);
+
+            const resFile = await axios({
+                method: "post",
+                url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
+                data: formData,
+                headers: {
+                    'pinata_api_key': `81852fb3d69cda1abf17`, // ${process.env.REACT_APP_PINATA_API_KEY}
+                    'pinata_secret_api_key': `d0621cab7ea068f42c4c150be07b14a99a91be4f4de700fe82746704e12acbf6`, // ${process.env.REACT_APP_PINATA_API_SECRET}
+                    "Content-Type": "multipart/form-data"
+                },
+            });
+
+            const ImgHash = `ipfs://${resFile.data.IpfsHash}`;
+        console.log(ImgHash); 
+    //Take a look at your Pinata Pinned section, you will see a new file added to you list.   
+
+
+
+        } catch (error) {
+            console.log("Error sending File to IPFS: ")
+            console.log(error)
+        }
+    }
+  }
+// // infura, goerli,
+// const onSubmitHandler = async (event) => {
+//   event.preventDefault();
+//   const form = event.target;
+//   const files = (form[0] ).files;
+
+//   if (!files || files.length === 0) {
+//     return alert("No files selected");
+//   }
+
+//   const file = files[0];
+//   // upload files
+
+//  console.log(result);
+
+//   form.reset();
+// };
+
+  return(
+    <form onSubmit={e => { e.preventDefault(); sendFileToIPFS() } }>
+    <input type="file" onChange={(e) =>setFileImg(e.target.files[0])} required />
+    <button type='submit' >Mint NFT</button>            
+    </form>
+    )
+}
+
+export default App;
